4c5044ad284dbbcdd134ab83ded70751c660b68f
Hunter305 <avinash8169@gmail.com>
2023-08-02 19:41:58 +0530

    portfolio website 99%

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,6 +12,7 @@ import React from "react";
 import procart from "../img/procart.png";
 import urls from "../img/urls.png";
 import mesmer from "../img/mesmer.png";
+import tracky from "../img/tracky.png";
 
 const Projects = () => {
   return (
@@ -189,45 +190,41 @@ const Projects = () => {
           </Grid>
         </Grid>
         <Grid item xs={12}>
-          <h4 style={{ marginBottom: "0px", marginTop: "10px" }}>
-            Url Shortner
-          </h4>
+          <h4 style={{ marginBottom: "0px", marginTop: "10px" }}>Tracky</h4>
 
           <Grid container spacing={4}>
             <Grid item xs={12} md={6} sx={{ border: "solid" }}>
               <p>
-                URL Shortener is an innovative web application built using the
-                MERN stack (MongoDB, Express.js, React, Node.js) that simplifies
-                long URLs into short, shareable links. As a passionate web
-                developer, I conceptualized and developed this project to
-                enhance user experience and make link sharing more convenient.
+                Tracky is a dynamic job tracking web application built using the
+                MERN stack (MongoDB, Express.js, React, and Node.js). As a
+                passionate web developer, I envisioned and developed Tracky to
+                streamline the job search process and empower users to
+                efficiently track their job applications in one centralized
+                platform.
               </p>
               <p>
-                URL Shortener showcases my skills in full-stack web development,
-                demonstrating my ability to create practical, user-centric
-                applications using cutting-edge technologies. It exemplifies my
-                commitment to delivering high-quality web solutions to
-                real-world challenges.
+                During the development of Tracky, my focus was on creating a
+                powerful yet user-friendly application that simplifies the job
+                search process and empowers users to take control of their job
+                applications. I ensured that the application's design was
+                visually appealing and intuitive, aiming for a seamless user
+                experience.
               </p>
             </Grid>
             <Grid item xs={12} md={6}>
               <Card sx={{ maxWidth: 345, marginBottom: "20px" }}>
-                <CardMedia
-                  sx={{ height: 170 }}
-                  image={urls}
-                  title="url shortner"
-                />
+                <CardMedia sx={{ height: 170 }} image={tracky} title="Tracky" />
                 <CardContent>
                   <Typography gutterBottom variant="h5" component="div">
-                    Url Shortner
+                    Tracky
                   </Typography>
                   <Typography variant="body2" color="text.secondary">
-                    Shorten your url using the webapp built using MERN stack.{" "}
+                    Track your job applications using tracky
                     <br />
                   </Typography>
                   Demo:
                   <ul style={{ marginTop: "0px" }}>
-                    <li>email:test@email.com, password: 1234</li>
+                    <li>email:test@email.com, password: test@1234</li>
                   </ul>
                 </CardContent>
                 <CardActions>
@@ -241,7 +238,7 @@ const Projects = () => {
                     }}
                   >
                     <a
-                      href="https://delightful-douhua-e5a8b9.netlify.app/"
+                      href="https://tracky.onrender.com/"
                       target="_blank"
                       style={{ textDecoration: "none", color: "black" }}
                     >
@@ -258,7 +255,7 @@ const Projects = () => {
                     }}
                   >
                     <a
-                      href="https://github.com/Hunter305/url_short_frontend/"
+                      href="https://github.com/Hunter305/tracky"
                       target="_blank"
                       style={{ textDecoration: "none", color: "black" }}
                     >
@@ -270,6 +267,78 @@ const Projects = () => {
             </Grid>
           </Grid>
         </Grid>
+        {/* <Grid item xs={12} sx={{ marginBottom: "50px" }}>
+          <h4 style={{ marginBottom: "20px", marginTop: "40px" }}>Mesmer</h4>
+          <Grid container spacing={4}>
+            <Grid item xs={12} md={6}>
+              <Card sx={{ maxWidth: 345, marginBottom: "20px" }}>
+                <CardMedia sx={{ height: 170 }} image={mesmer} title="mesmer" />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Mesmer
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Just get mesmerized in the music
+                    <br />
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button
+                    variant="contained"
+                    size="small"
+                    sx={{
+                      backgroundColor: "#69CF95",
+                      color: "#000",
+                      "&:hover": { backgroundColor: "#69CF95" },
+                    }}
+                  >
+                    <a
+                      href="https://mesmermusic.netlify.app/"
+                      target="_blank"
+                      style={{ textDecoration: "none", color: "black" }}
+                    >
+                      Launch app
+                    </a>
+                  </Button>
+                  <Button
+                    variant="contained"
+                    size="small"
+                    sx={{
+                      backgroundColor: "#69CF95",
+                      color: "#000",
+                      "&:hover": { backgroundColor: "#69CF95" },
+                    }}
+                  >
+                    <a
+                      href="https://github.com/Hunter305/Mesmer"
+                      target="_blank"
+                      style={{ textDecoration: "none", color: "black" }}
+                    >
+                      Code
+                    </a>
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+            <Grid item xs={12} md={6} sx={{ border: "solid" }}>
+              <p>
+                Mesmer is an engaging and immersive music player web application
+                built using React and powered by the Shazam API. As a passionate
+                web developer and music enthusiast, I conceptualized and created
+                Mesmer to deliver a captivating and seamless music listening
+                experience to users.
+              </p>
+              <p>
+                Mesmer exemplifies my proficiency in leveraging cutting-edge
+                technologies like React and integrating external APIs to create
+                interactive and feature-rich web applications. As a music player
+                that provides a captivating and personalized listening
+                experience, Mesmer is a testament to my passion for delivering
+                innovative solutions through web development.
+              </p>
+            </Grid>
+          </Grid>
+        </Grid> */}
       </Grid>
     </Container>
   );
